505131dce877327d62636f6f34a4e1209e861208
Yannik Höll <labruzzler@gmail.com>
2021-09-30 20:44:35 +0200

    tex

--- rbtree/src/main.c
+++ rbtree/src/main.c
@@ -9,17 +9,19 @@
 // benchmark output format: CSV insert_rb, search_rb, delete_rb, height_rb, insert_bin, search_bin, delete_bin, height_bin
 
 // source: https://stackoverflow.com/questions/6749621/how-to-create-a-high-resolution-timer-in-linux-to-measure-program-performance
-struct timespec diff(struct timespec start, struct timespec end)
+struct timespec timer_start()
 {
-    struct timespec temp;
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-    }
-    return temp;
+    struct timespec start_time;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+    return start_time;
+}
+
+long timer_end(struct timespec start_time)
+{
+    struct timespec end_time;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+    long diffInNanos = (end_time.tv_sec - start_time.tv_sec) * (long)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
+    return diffInNanos;
 }
 
 T* malloc_elem(T elem) {
@@ -50,65 +52,59 @@ void gen_degenerate_data(int *keys, int *searches, size_t number_of_keys)
 
 void test_rbtree(int keys[], int searches[], size_t arr_size) {
     struct RBTree *rbtree = create_tree();
-    struct timespec time1, time2;
+    struct timespec time1;
     
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (size_t u = 0; u < arr_size; u++) {
         int *elem = (int*)malloc(sizeof(int));
         *elem = keys[u];
 
         insert_node(rbtree, elem, NULL);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
     
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (size_t i = 0; i < arr_size; i++) {
         struct Node* node;
         search_node(rbtree, &searches[i], &node);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
     
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (int u = arr_size - 1; u >= 0; u--) {
         int key = rand() % arr_size;
         delete_node(rbtree, &key);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
     
     printf("%zu ", find_tree_height(rbtree->root, 0));
 }
 
 void test_bin_tree(int keys[], int searches[], size_t arr_size) {
     struct BinTree *binTree = create_bin_tree();
-    struct timespec time1, time2;
+    struct timespec time1;
     
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (size_t u = 0; u < arr_size; u++) {
         int *elem = (int*)malloc(sizeof(int));
         *elem = keys[u];
 
         insert_bin_node(binTree, elem);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (size_t i = 0; i < arr_size; i++) {
         search_bin(binTree, &searches[i]);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    time1 = timer_start();
     for (int u = arr_size - 1; u >= 0; u--) {
         int key = rand() % arr_size;
         delete_bin_node(binTree, &key);
     }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    printf("%ld ", diff(time1, time2).tv_nsec);
+    printf("%ld ", timer_end(time1));
 
     printf("%zu ", find_tree_height_bin(binTree->root, 0));
 }
@@ -130,7 +126,10 @@ void benchmark(size_t number_of_keys)
 
 int main(int argc, char** argv)
 {   
-    if (argc < 2) exit(EXIT_FAILURE);
+    if (argc < 2) {
+        printf("Usage: ./rbtree.out <number-of-keys>\n");
+        exit(EXIT_FAILURE);
+    }
     
     srand(time(NULL));
 
