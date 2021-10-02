21d2828ff1c33154208fc085247111271f2d2189
KoKoKotlin <labruzzler@gmail.com>
2021-10-03 00:28:52 +0200

    benchmarking

--- rbtree/benchmark.py
+++ rbtree/benchmark.py
@@ -1,7 +1,12 @@
+from collections import defaultdict
 from enum import Enum, auto
 import os
 import subprocess
 import time
+
+from typing import List, Dict
+from functools import reduce
+from pprint import pprint
 
 class VERSION(Enum):
     NON_REC = auto()
@@ -16,24 +21,84 @@ def compile_version(version):
 def execute(number_of_keys):
     print(f"Running benchmark for {number_of_keys} keys!")
     res = subprocess.run(["./src/rbtree.out", str(number_of_keys)], capture_output=True)
-    return ','.join(res.stdout.decode('ascii').strip().split(' '))
+    return list(map(int, res.stdout.decode('ascii').strip().split(' ')))
+
+def write_to_file(data: Dict[int, List[float]], mode: str):
+    with open(f"benchmark/benchmark_{mode}_{time.time()}.csv", "w") as f:
+        f.write("no keys,insert_{rb},search_{rb},delete_{rb},height_{rb},insert_{bin},search_{bin},delete_{bin},height_{bin}\n")
+        for key, val in data.items():
+            f.write(",".join(map(str, [key] + val)) + "\n")
+
+class Batch:
+    def __init__(self, id):
+        self.measurements = dict()
+        self.id = id
+
+    def add_mesurement(self, no_nodes, data):
+        self.measurements[no_nodes] = data
+
+    def __repr__(self) -> str:
+        output = f"Batch {self.id}:\n"
+        for key, val in self.measurements.items():
+            output += f"{key}: {val}\n"
+
+        return output
+
+def add_lists(l1, l2):
+    assert len(l1) == len(l2), "Error in List Dimensions!"
+    return [l1[i] + l2[i] for i in range(len(l1))]
+
+def avg_data(batches: List[Batch]) -> Dict[int, List[float]]:
+
+    # TODO: make list lenght a constamt
+    avg = defaultdict(lambda: [0] * 8)
+
+    # add all measurements up, that have the same number of nodes
+    for batch in batches:
+        for key, val in batch.measurements.items():
+            avg[key] = add_lists(avg[key], list(map(lambda x: x / len(batches), val)))
+
+    return avg
 
 def main():
-    output = []
     start = 100_000
     stop  = 200_000
     step  = 10_000
-    
+    batch_size = 10
+
     compile_version(VERSION.NON_REC)
-    output.extend([f"REC,{i}," + execute(i) for i in range(start, stop + 1, step)])
+    batches = []
+    for i in range(batch_size):
+        print(f"batch {i}:")
+        current_batch = Batch(i)
+        for nodes in range(start, stop + 1, step):
+            current_batch.add_mesurement(nodes, execute(nodes))
+
+        batches.append(current_batch)
+
+    print("Results: ")
+    print(batches)
+
+    avg = avg_data(batches)
+    pprint(avg)
+    write_to_file(avg, "NON_REC")
 
     compile_version(VERSION.REC)
-    output.extend([f"NON_REC,{i}," + execute(i) for i in range(start, stop + 1, step)])
+    batches = []
+    for i in range(batch_size):
+        print(f"batch {i}:")
+        current_batch = Batch(i)
+        for nodes in range(start, stop + 1, step):
+            current_batch.add_mesurement(nodes, execute(nodes))
 
-    with open(f"benchmark/benchmark{time.time()}.csv", "w") as f:
-        f.write("mode,number_of_keys,insert_rb,search_rb,delete_rb,height_rb,insert_bin,search_bin,delete_bin,height_bin\n")
-        f.write("\n".join(output))
+        batches.append(current_batch)
 
+    print("Results: ")
+    print(batches)
+
+    avg = avg_data(batches)
+    pprint(avg)
+    write_to_file(avg, "REC")
 
 if __name__ == "__main__":
     main()
--- rbtree/benchmark/main.gnuplot
+++ rbtree/benchmark/main.gnuplot
@@ -1,12 +1,7 @@
-set datafile separator ','
-set xdata time
-set timefmt "%Y-%m-%dT%H:%M:%S"
 set key autotitle columnhead
-set ylabel "First Y Units" 
-set xlabel 'Time'
+set datafile separator ','
 
-set y2tics # enable second axis
-set ytics nomirror # dont show the tics on that side
-set y2label "Second Y Axis Value" # label for second axis
+set xlabel "number of nodes"
 
-plot b.csv using 1:2 with lines, '' using 1:3 with lines, '' using 1:4 with lines axis x1y2
+# plot the rb insert time
+plot "b.csv" using 1:2 with lines, "b.csv" using 1:3 with lines, "b.csv" using 1:4 with lines, "b.csv" using 1:6 with lines, "b.csv" using 1:7 with lines, "b.csv" using 1:8 with lines
